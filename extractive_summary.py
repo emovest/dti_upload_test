6fff492db71562da963107a8cf18f87756ffdfb6
emovest <xrayenglish@gmail.com>
2025-07-22 14:22:44 -0400

    Create extractive_summary.py

---
+++ extractive_summary.py
@@ -0,0 +1,11 @@
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+# 提取式摘要函数
+def extractive_summary_sumy(abstracts_list, num_sentences=5):
+    full_text = " ".join(abstracts_list)
+    parser = PlaintextParser.from_string(full_text, Tokenizer("english"))
+    summarizer = LexRankSummarizer()
+    summary = summarizer(parser.document, num_sentences)
+    return " ".join(str(sentence) for sentence in summary)
